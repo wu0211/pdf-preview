d88f124b3be9f57b2e767fe1cea7ff08210d8de1
wu0211 <wbs_211@163.com>
2022-05-17 22:59:22 +0800

    dist

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="height: 95vh">
+    <PDFPreview :datas="datas" :startPage="1" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  setup() {
+    const datas = [
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/0.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/1.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/2.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/3.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/4.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/5.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/6.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/7.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/8.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/9.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/10.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/11.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/12.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/13.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/14.png",
+      "https://wu-public.oss-cn-beijing.aliyuncs.com/pdf-preview/15.png",
+    ];
+
+    return {
+      datas,
+    };
+  },
+};
 </script>
 
 <style>
@@ -21,6 +42,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PDFPreview from "pdf-preview-vue3";
+import 'pdf-preview-vue3/style.css'
+createApp(App).use(PDFPreview).mount('#app')
 
-createApp(App).mount('#app')
