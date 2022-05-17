df66c4d4b97c529bd47b881c909bf4ca3c701f45
wu0211 <wbs_211@163.com>
2022-05-17 23:10:27 +0800

    demo

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PDFPreview from "pdf-preview-vue3";
-import 'pdf-preview-vue3/style.css'
-createApp(App).use(PDFPreview).mount('#app')
-
